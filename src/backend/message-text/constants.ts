b1d053941e968b22278faec4e060765d3107c140
qpwedev <russfdk@gmail.com>
2023-05-13 13:49:00 +0100

    main menu text update

--- src/backend/message-text/constants.ts
+++ src/backend/message-text/constants.ts
@@ -1,15 +1,14 @@
-const mainMenuText = `🏕 <u><b>Welcome to our Carbon Offset App</b></u>
+const mainMenuText = `🏕 <u><b>Welcome to ReCO2</b></u>
 
 With this app, you can measure your daily carbon footprint and offset it using cryptocurrency. In three easy steps, you can contribute to a <b>greener future</b>:
 
 1. Log your daily activities.
 2. Get an instant calculation of your CO2 emissions.
-3. Connect wallet and offset your carbon footprint with crypto.
+3. Connect your wallet and offset your carbon footprint with crypto.
 
 <i>Our app turns your everyday choices into opportunities for environmental stewardship. Make a positive impact with each decision, and use advanced technology to help the planet</i>.
 
 <b><span class="tg-spoiler">Start your journey towards sustainability now</span> 🚀</b>`;
-
 
 const webAppStartBtnText = '🏕 Start ReCO2';
 

b82d1e58d495e7db24242605664b518c94fbcd25
qpwedev <russfdk@gmail.com>
2023-05-13 11:39:33 +0100

    init

---
+++ src/backend/main.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Telegraf } from 'telegraf'
+
+const bot = new Telegraf(process.env.BOT_TOKEN!)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
